5da999969b73963ba29b649c24a4861510706869
Tehqo <immantablas@gmail.com>
2021-03-03 15:23:37 -0500

    Added Reactions Roles for Programs

--- README.md
+++ README.md
@@ -15,3 +15,6 @@ The only departments available are the biology and chemistry departments. Doing 
 
 *\>clear [number of messages]*  
 Will clear a specified amount of messages sent prior to the usage of the command.
+
+*\>rolemenu*
+Will open a role menu with relevant programs in which students can react to the message with an emoji to assign themselves to a program.
--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def enroll(ctx, *args):
     for role in rolelist:
         if role.name == classes:
             await ctx.author.add_roles(role)
-            await ctx.send(f"You are now enrolled in {role}.")
+            await ctx.message.author.send(f"You are now enrolled in {role}.")
 
 @client.command()
 async def drop(ctx, *args):
@@ -38,7 +38,81 @@ async def drop(ctx, *args):
     for role in rolelist:
         if role.name == classes:
             await ctx.author.remove_roles(role)
-            await ctx.send(f"You dropped {role}")
+            await ctx.message.author.send(f"You dropped {role}")
+
+@client.command()
+@commands.is_owner()
+async def rolemenu(ctx):
+    message = await ctx.send("React to give yourself a role.\n \n :magnet: : `Physics`\n \n :brain: : `Behavioural Neuroscience` \n \n :test_tube: : `Chemistry` \n \n :atom: : `Biochemistry` \n \n :microscope: : `Biology` \n \n :star: : `Other`")
+    emojilist = ["🧲", "🧠", "🧪", "⚛", "🔬", "⭐"]
+    for emoji in emojilist:
+        await message.add_reaction(emoji)
+
+@client.event
+async def on_reaction_add(reaction, user):
+    emoji = reaction.emoji
+
+
+    channelID = 799458309501091861
+    if reaction.message.channel.id != channelID:
+        return
+
+    Physics = discord.utils.get(user.guild.roles, name = "Physics")
+    BehavioralNeuroscience = discord.utils.get(user.guild.roles, name = "Behavioral Neuroscience")
+    Chemistry = discord.utils.get(user.guild.roles, name = "Chemistry")
+    Biochemistry = discord.utils.get(user.guild.roles, name = "Biochemistry")
+    Biology = discord.utils.get(user.guild.roles, name = "Biology")
+    Others = discord.utils.get(user.guild.roles, name = "Other Programs")
+
+    if user.bot:
+        return
+    
+    if emoji == "🧲":
+        await user.add_roles(Physics)
+    if emoji == "🧠":
+        await user.add_roles(BehavioralNeuroscience)
+    if emoji == "🧪":
+        await user.add_roles(Chemistry)
+    if emoji == "⚛":
+        await user.add_roles(Biochemistry)
+    if emoji == "🔬":
+        await user.add_roles(Biology)
+    if emoji == "⭐":
+        await user.add_roles(Others)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    emoji = payload.emoji
+    guild = await client.fetch_guild(payload.guild_id)
+    user = await guild.fetch_member(payload.user_id)
+    
+
+    channelID = 799458309501091861
+    if payload.channel_id != channelID:
+        return
+
+    Physics = discord.utils.get(guild.roles, name = "Physics")
+    BehavioralNeuroscience = discord.utils.get(guild.roles, name = "Behavioral Neuroscience")
+    Chemistry = discord.utils.get(guild.roles, name = "Chemistry")
+    Biochemistry = discord.utils.get(guild.roles, name = "Biochemistry")
+    Biology = discord.utils.get(guild.roles, name = "Biology")
+    Others = discord.utils.get(guild.roles, name = "Other Programs")
+
+    if user.bot:
+        return
+    
+    if str(emoji) == "🧲":
+        await user.remove_roles(Physics)
+    if str(emoji) == "🧠":
+        await user.remove_roles(BehavioralNeuroscience)
+    if str(emoji) == "🧪":
+        await user.remove_roles(Chemistry)
+    if str(emoji) == "⚛":
+        await user.remove_roles(Biochemistry)
+    if str(emoji) == "🔬":
+        await user.remove_roles(Biology)
+    if str(emoji) == "⭐":
+        await user.remove_roles(Others)
 
 
 
